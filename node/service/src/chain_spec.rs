824fc88c5ad79d52f0d36b153b8735aa16bc614c
Cheng JIANG <alex_cj96@foxmail.com>
2021-11-04 10:06:47 +0800

    add pallet-sudo for kusama

--- node/service/src/chain_spec.rs
+++ node/service/src/chain_spec.rs
@@ -758,6 +758,7 @@ fn kusama_staging_testnet_config_genesis(wasm_binary: &[u8]) -> kusama::GenesisC
 		gilt: Default::default(),
 		paras: Default::default(),
 		xcm_pallet: kusama::XcmPalletConfig { safe_xcm_version: Some(2) },
+		sudo: kusama::SudoConfig { key: endowed_accounts[0].clone() },
 	}
 }
 
@@ -1345,7 +1346,7 @@ pub fn kusama_testnet_genesis(
 		AssignmentId,
 		AuthorityDiscoveryId,
 	)>,
-	_root_key: AccountId,
+	root_key: AccountId,
 	endowed_accounts: Option<Vec<AccountId>>,
 ) -> kusama::GenesisConfig {
 	let endowed_accounts: Vec<AccountId> = endowed_accounts.unwrap_or_else(testnet_accounts);
@@ -1426,6 +1427,7 @@ pub fn kusama_testnet_genesis(
 		gilt: Default::default(),
 		paras: Default::default(),
 		xcm_pallet: kusama::XcmPalletConfig { safe_xcm_version: Some(2) },
+		sudo: kusama::SudoConfig { key: root_key },
 	}
 }
 
--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -977,6 +977,11 @@ impl pallet_society::Config for Runtime {
 	type PalletId = SocietyPalletId;
 }
 
+impl pallet_sudo::Config for Runtime {
+	type Event = Event;
+	type Call = Call;
+}
+
 parameter_types! {
 	pub const MinVestedTransfer: Balance = 100 * CENTS;
 }
@@ -1533,6 +1538,9 @@ construct_runtime! {
 
 		// Pallet for sending XCM.
 		XcmPallet: pallet_xcm::{Pallet, Call, Storage, Event<T>, Origin, Config} = 99,
+
+		// Sudo.
+		Sudo: pallet_sudo::{Pallet, Call, Storage, Event<T>, Config<T>} = 100,
 	}
 }
 
